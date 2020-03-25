e090e5b2eb1ca90dff0239266b5abaf6dfd80077
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-25 17:45:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,22 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    
+    if user.username != '' && user.save 
+      redirect to('/login')
+    else 
+      redirect to('/failure')
+    end
+  end
+  
+  get '/withdraw' do 
+    @user = User.find(session[:user_id])
+    if @user.balance == 0 
+      erb :failure
+    else 
+      erb :withdraw 
+    end
   end
 
   get '/account' do
@@ -33,6 +48,14 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+ 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do

f2d4eb3c6dc98d774bed4e189c85b2dee30d0a1c
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-25 18:05:32 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  after_initialize :init
+
+  def init
+    self.number  ||= 0.0           #will set the default value only if it's nil
+    self.address ||= build_address #let's you set a default association
+  end
 end
